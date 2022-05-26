cbdc0bff3317d1752a60f083b640cba5bf0a1600
Monica Ivanova <98054187+ivanova81@users.noreply.github.com>
2022-05-26 22:42:19 +0300

    Add project files.

---
+++ StudentsExample - POM/Pages/AddStudentPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriver_POM_Example.Pages
+{
+    public class AddStudentPage : BasePage
+    {
+        public AddStudentPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+        public override string PageUrl => "https://mvc-app-node-express.nakov.repl.co/add-student";
+        public IWebElement InputName => driver.FindElement(By.Id("name"));
+        public IWebElement EmailName => driver.FindElement(By.Id("email"));
+        public IWebElement AddButton => driver.FindElement(By.CssSelector("button[type='submit']"));
+
+        public IWebElement ErrorMessage => driver.FindElement(By.CssSelector("body > div"));
+
+        public void AddStudent(string name, string email)
+        {
+            this.InputName.SendKeys(name);
+            this.EmailName.SendKeys(email);
+            this.AddButton.Click();
+        }
+    }
+}
---
+++ StudentsExample - POM/Tests/AddStudentTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriver_POM_Example.Pages;
+
+namespace WebDriver_POM_Example.Tests
+{
+    public class AddStudentTests : BaseTests
+    {
+        [Test]
+        public void Test_AddStudentPage_TitleAndHeading()
+        {
+            var add_Student = new AddStudentPage(driver);
+            add_Student.Open();
+            Assert.That(add_Student.GetPageTitle(), Is.EqualTo("Add Student"));
+            Assert.That(add_Student.GetPageHeadingTexts(), Is.EqualTo("Register New Student"));
+        }
+
+        [Test]
+        public void Test_AddStudentPage_EmptyFields()
+        {
+            var add_Student = new AddStudentPage(driver);
+            add_Student.Open();
+            Assert.That(add_Student.EmailName.Text, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void Test_AddStudentPage_AddValidStudent()
+        {
+            var add_Student = new AddStudentPage(driver);
+            add_Student.Open();
+            string name = "David" + DateTime.Now.Ticks;
+            string email = "David" + DateTime.Now.Ticks + "@gmail.com";
+            add_Student.AddStudent(name, email);
+            var view_student = new ViewStudentsPage(driver);
+            Assert.IsTrue(view_student.isOpen());
+            var students = view_student.GetRegisteredStudents();
+            var lastStudent = students.Last();
+            string expected = name + " (" + email + ")";
+            Assert.Contains(lastStudent, students);
+        }
+
+        [Test]
+        public void Test_AddStudentPage_AddInValidStudent()
+        {
+            var add_Student = new AddStudentPage(driver);
+            add_Student.Open();
+            string name = "";
+            string email = "";
+            add_Student.AddStudent(name, email);
+            Assert.IsTrue(add_Student.isOpen());
+            Assert.That(add_Student.ErrorMessage.Text, Is.EqualTo("Cannot add student. Name and email fields are required!"));
+        }
+    }
+}
