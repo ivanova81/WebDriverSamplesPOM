89907c733d6bbd4beb303740a798ca8d10c4469d
Monica Ivanova <98054187+ivanova81@users.noreply.github.com>
2022-05-26 22:44:06 +0300

    Add project files.

---
+++ StudentsProject - POM/Tests/BaseTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriver_POM_Example.Tests
+{
+    public class BaseTests
+    {
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            this.driver = new ChromeDriver();
+        }
+
+        [OneTimeTearDown]
+        public void ShutdownBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
